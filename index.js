88d796071ccdf81f1142b20e0df96886555c4a49
JED <jedsada.s@dotography.com>
2018-05-27 02:18:24 +0700

    init project

---
+++ index.js
@@ -0,0 +1,5 @@
+import { AppRegistry } from 'react-native';
+import { RemoteMessage } from 'react-native-firebase';
+import App from './App';
+
+AppRegistry.registerComponent('msgNotiApp', () => App);
